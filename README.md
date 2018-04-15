502985cfde202a9b20e470a82c57064d3a300e23
fanyoufu <94031570@qq.com>
2018-04-15 20:09:03 +0800

    'excel操作'

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # vue-util
 工具
+## 方法
+```
+this._excel.download(aDom,data, downName)
+```
+1. aDom:页面中的某个a标签结点.
+2. data:要下载的数据。结构如下：
+[{id:'编号',name:'姓名',age:"年龄"},
+{id:'01',name:'小凡',age:"10"},]
+第一行是表头。
+3. downName文件名
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import common from "./common.js"
-
+import excel from "./excel.js"
 export default {
-  ...common,
   install : function (Vue, options) {
-    Vue.prototype._util = common
+    Vue.prototype._util = common;
+    Vue.prototype._excel = excel
   }
 }
